04cc23ea1cf846779b6566a23ea65d6ef91f769e
neozhaoliang <mathzhaoliang@gmail.com>
2017-03-05 14:53:56 +0800

    add mouse control

--- src/grayscott/main.py
+++ src/grayscott/main.py
@@ -1,3 +1,4 @@
+# pylint: disable = unused-variable
 '''
 A GrayScott reaction-diffusion simulator written with pyglet and GLSL.
 
@@ -20,28 +21,6 @@ from shader import Shader
 from framebuffer import FrameBuffer
 
 
-# pattern: [rU, rV, feed, kill]
-species = { 'spots_and_loops': [0.2097, 0.105, 0.018, 0.051],
-            'zebrafish':       [0.16, 0.08, 0.035, 0.060],
-            'solitons':        [0.14, 0.06, 0.035, 0.065],
-            'coral':           [0.16, 0.08, 0.060, 0.062],
-            'fingerprint' :    [0.19, 0.05, 0.060, 0.062],
-            'worms':           [0.16, 0.08, 0.050, 0.065],
-}
-
-# palette will be used for coloring the uv_texture.
-palette = [(0.0, 0.0, 0.0, 0.0),
-           (0.0, 1.0, 0.0, 0.2),
-           (1.0, 1.0, 0.0, 0.21),
-           (1.0, 0.0, 0.0, 0.4),
-           (1.0, 1.0, 1.0, 0.6)]
-
-# we will need two shaders, one for computing the reaction and diffusion,
-# and one for coloring the uv_texture.
-reaction_shader = Shader.from_files('reaction.vert', 'reaction.frag')
-render_shader = Shader.from_files('render.vert', 'render.frag')
-
-
 def create_texture_from_array(array):
     '''
     create a pyglet texture instance from a numpy ndarray.
@@ -55,105 +34,181 @@ def create_texture_from_array(array):
     return texture
 
 
-def GrayScott(width, height, scale, pattern):
+class GrayScott(pyglet.window.Window):
+
     '''
-    width, height:
-        size of the window in pixels.
+    Kerboard and mouse control:
+        1. press 'Enter' to take screenshots.
+        2. click or drag your mouse to disturb the pattern.
+        3. press 'Esc' to exit.
+    '''
+
+    # pattern: [rU, rV, feed, kill]
+    species = { 'spots_and_loops': [0.2097, 0.105, 0.018, 0.051],
+                'zebrafish':       [0.16, 0.08, 0.035, 0.060],
+                'solitons':        [0.14, 0.06, 0.035, 0.065],
+                'coral':           [0.16, 0.08, 0.060, 0.062],
+                'fingerprint' :    [0.19, 0.05, 0.060, 0.062],
+                'worms':           [0.16, 0.08, 0.050, 0.065],
+    }
+
+    # palette will be used for coloring the uv_texture.
+    palette = [(0.0, 0.0, 0.0, 0.0),
+               (0.0, 1.0, 0.0, 0.2),
+               (1.0, 1.0, 0.0, 0.21),
+               (1.0, 0.0, 0.0, 0.4),
+               (1.0, 1.0, 1.0, 0.6)]
     
-    scale:
-        determines the size of the grid.
-        the grid will be of size (width//scale) x (height//scale)
-
-    pattern:
-        must be one of 'solitons', 'worms', 'spots_and_loops', etc. 
-    '''
-    window = pyglet.window.Window(width, height, caption='GrayScott Simulation',
-                                  visible=False, vsync=False)
-    window.set_location(100, 100)
-    width //= scale
-    height //= scale
-
-    # now we set the uniforms and attributes in the two shaders.
-    rU, rV, feed, kill = species[pattern]
-    with reaction_shader:
-        reaction_shader.set_uniformi('uv_texture', 0)
-        reaction_shader.set_uniformf('dx', 1.0/width)
-        reaction_shader.set_uniformf('dy', 1.0/height)
-        reaction_shader.set_uniformf('feed', feed)
-        reaction_shader.set_uniformf('kill', kill)
-        reaction_shader.set_uniformf('rU', rU)
-        reaction_shader.set_uniformf('rV', rV)
-        # the order of the vertices and texcoords matters,
-        # since we will call 'GL_TRIANGLE_STRIP' to draw them.
-        reaction_shader.set_vertex_attrib('position', [(-1, -1), (1, -1), (-1, 1,), (1, 1)])
-        reaction_shader.set_vertex_attrib('texcoord', [(0, 0), (1, 0), (0, 1), (1, 1)])
-
-    with render_shader:
-        render_shader.set_uniformi('uv_texture', 0)
-        render_shader.set_uniformf('color1', *palette[0])
-        render_shader.set_uniformf('color2', *palette[1])
-        render_shader.set_uniformf('color3', *palette[2])
-        render_shader.set_uniformf('color4', *palette[3])
-        render_shader.set_uniformf('color5', *palette[4])
-        render_shader.set_vertex_attrib('position', [(-1, -1), (1, -1), (-1, 1), (1, 1)])
-        render_shader.set_vertex_attrib('texcoord', [(0, 0), (1, 0), (0, 1), (1, 1)])
-
-    uv_grid = np.zeros((height, width, 4), dtype=np.float32)
-    uv_grid[:, :, 0] = 1.0
-    r = 32
-    uv_grid[height//2-r : height//2+r,  width//2-r : width//2+r,  0] = 0.50
-    uv_grid[height//2-r : height//2+r,  width//2-r : width//2+r,  1] = 0.25
-    uv_texture = create_texture_from_array(uv_grid)
-    # the texture is bind to unit '0'.
-    gl.glActiveTexture(gl.GL_TEXTURE0)
-    gl.glBindTexture(uv_texture.target, uv_texture.id)
-
-    # we need a framebuffer to do the offscreen rendering.
-    # once we finished computing the reaction-diffusion step with the reaction_shader,
-    # we render the result to this 'invisible' buffer since we do not want to show it.
-    # the final image is further colored by the render_shader and will be rendered to the window.
-    with FrameBuffer() as fbo:
-        fbo.attach_texture(uv_texture)
-
-    @window.event
-    def on_key_press(symbol, _):
+    def __init__(self, width, height, scale, pattern):
         '''
-        prese 'Enter' to take snapshots.
+        width, height:
+            size of the window in pixels.
+        scale:
+            the size of the texture is (width//scale) x (height//scale).
+        pattern:
+            a name in dict 'species'.
         '''
-        if symbol == pyglet.window.key.ENTER:
-            index = np.random.randint(0, 1000)
-            pyglet.image.get_buffer_manager().get_color_buffer().save('screenshot{:03d}.png'.format(index))
+        pyglet.window.Window.__init__(self, width, height, caption='GrayScott Simulation',
+                                      visible=False, vsync=False)
 
-    @window.event
-    def on_draw():
-        gl.glClearColor(0, 0, 0, 0)
-        window.clear()
-        # since we are rendering to the invisible framebuffer, the size is just the texture's size. 
+        # we will need two shaders, one for computing the reaction and diffusion,
+        # and one for coloring the uv_texture.
+        self.reaction_shader = Shader.from_files('default.vert', 'reaction.frag')
+        self.render_shader = Shader.from_files('default.vert', 'render.frag')
+        self.tex_width = width // scale
+        self.tex_height = height // scale
+        self.uv_texture = self.create_texture(width//scale, height//scale)
+
+        # set the uniforms and attributes in the two shaders.
+        self.set_shader_data(pattern)
+
+        # we need a framebuffer to do the offscreen rendering.
+        # once we finished computing the reaction-diffusion step with the reaction_shader,
+        # we render the result to this 'invisible' buffer since we do not want to show it.
+        # the final image is further colored by the render_shader and will be rendered to the window.
+        with FrameBuffer() as self.fbo:
+            self.fbo.attach_texture(self.uv_texture)
+
+        # why do we need this?
+        # the reason is in the 'on_mouse_drag' function. 
+        self.mouse_down = False
+
+
+    def create_texture(self, width, height):
+        uv_grid = np.zeros((height, width, 4), dtype=np.float32)
+        uv_grid[:, :, 0] = 1.0
+        r = 32
+        uv_grid[height//2-r : height//2+r,  width//2-r : width//2+r,  0] = 0.50
+        uv_grid[height//2-r : height//2+r,  width//2-r : width//2+r,  1] = 0.25
+        uv_texture = create_texture_from_array(uv_grid)
+        # the texture is bind to unit '0'.
+        gl.glActiveTexture(gl.GL_TEXTURE0)
+        gl.glBindTexture(uv_texture.target, uv_texture.id)
+        return uv_texture
+
+
+    def set_shader_data(self, pattern):
+        rU, rV, feed, kill = GrayScott.species[pattern]
+        color1, color2, color3, color4, color5 = GrayScott.palette
+        
+        with self.reaction_shader:
+            self.reaction_shader.set_uniformi('uv_texture', 0)
+            self.reaction_shader.set_uniformf('dx', 1.0/self.tex_width)
+            self.reaction_shader.set_uniformf('dy', 1.0/self.tex_height)
+            self.reaction_shader.set_uniformf('feed', feed)
+            self.reaction_shader.set_uniformf('kill', kill)
+            self.reaction_shader.set_uniformf('rU', rU)
+            self.reaction_shader.set_uniformf('rV', rV)
+            self.reaction_shader.set_uniformf('brush', -1, -1)
+            # the order of the vertices and texcoords matters,
+            # since we will call 'GL_TRIANGLE_STRIP' to draw them.
+            self.reaction_shader.set_vertex_attrib('position', [(-1, -1), (1, -1), (-1, 1,), (1, 1)])
+            self.reaction_shader.set_vertex_attrib('texcoord', [(0, 0), (1, 0), (0, 1), (1, 1)])
+
+        with self.render_shader:
+            self.render_shader.set_uniformi('uv_texture', 0)
+            self.render_shader.set_uniformf('color1', *color1)
+            self.render_shader.set_uniformf('color2', *color2)
+            self.render_shader.set_uniformf('color3', *color3)
+            self.render_shader.set_uniformf('color4', *color4)
+            self.render_shader.set_uniformf('color5', *color5)
+            self.render_shader.set_vertex_attrib('position', [(-1, -1), (1, -1), (-1, 1), (1, 1)])
+            self.render_shader.set_vertex_attrib('texcoord', [(0, 0), (1, 0), (0, 1), (1, 1)])
+
+
+    def set_view(self, width, height):
         gl.glViewport(0, 0, width, height)
         gl.glMatrixMode(gl.GL_PROJECTION)
         gl.glLoadIdentity()
         gl.glOrtho(0, 1, 0, 1, -1, 1)
         gl.glMatrixMode(gl.GL_MODELVIEW)
 
-        with fbo:
-            with reaction_shader:
+
+    def run(self):
+        self.set_visible(True)
+        pyglet.clock.schedule(lambda dt: None)
+        pyglet.app.run()
+
+
+    def on_draw(self):
+        gl.glClearColor(0, 0, 0, 0)
+        self.clear()
+
+        # since we are rendering to the invisible framebuffer, the size is just the texture's size.
+        self.set_view(self.tex_width, self.tex_height)
+        with self.fbo:
+            with self.reaction_shader:
                 gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 0, 4)
 
         # now we render to the actual window, hence the size is window's size.
-        gl.glViewport(0, 0, window.width, window.height)
-        gl.glMatrixMode(gl.GL_PROJECTION)
-        gl.glLoadIdentity()
-        gl.glOrtho(0, 1, 0, 1, -1, 1)
-        gl.glMatrixMode(gl.GL_MODELVIEW)
-
-        with render_shader:
+        self.set_view(self.width, self.height)
+        with self.render_shader:
             gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 0, 4)
 
-    pyglet.clock.schedule(lambda dt: None)
-    window.set_visible(True)
-    pyglet.app.run()
 
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.ENTER:
+            index = np.random.randint(0, 1000)
+            pyglet.image.get_buffer_manager().get_color_buffer().save('screenshot{:03d}.png'.format(index))
+
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        self.mouse_down = True
+        bx = x / float(self.width)
+        by = y / float(self.height)
+        self.set_view(self.tex_width, self.tex_height)
+        with self.fbo:
+            with self.reaction_shader:
+                self.reaction_shader.set_uniformf('brush', bx, by)
+                gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 0, 4)
+
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        self.mouse_down = False
+        bx = x / float(self.width)
+        by = y / float(self.height)
+        self.set_view(self.tex_width, self.tex_height)
+        with self.fbo:
+            with self.reaction_shader:
+                self.reaction_shader.set_uniformf('brush', -1, -1)
+                gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 0, 4)
+
+
+    def on_mouse_drag(self, x, y, dx, dy, button, modifiers):
+        '''
+        when dragging the mouse, it jumps between 'mouse up' and 'mouse down'.
+        '''
+        if self.mouse_down:
+            bx = (x + dx) / float(self.width)
+            by = (y + dy) / float(self.height)
+            self.set_view(self.tex_width, self.tex_height)
+            with self.fbo:
+                with self.reaction_shader:
+                    self.reaction_shader.set_uniformf('brush', bx, by)
+                    gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 0, 4)
+ 
 
 if __name__ == '__main__':
-    print('press \'ENTER\' to save snapshots, or \'ESC\' to exit')
-    GrayScott(600, 400, 2, 'solitons')
+    app = GrayScott(width=600, height=480, scale=2, pattern='zebrafish')
+    print(app.__doc__)
+    app.run()
--- src/grayscott/reaction.frag
+++ src/grayscott/reaction.frag
@@ -12,11 +12,18 @@ uniform float feed;
 uniform float kill;
 
 uniform sampler2D uv_texture;
+uniform vec2 brush;
 
 out vec4 result;
 
 void main()
 {
+    if(brush.x < -5.0)
+    {
+        result = vec4(1.0, 0.0, 0.0, 1.0);
+        return;
+    }
+    
     vec2 uv  = texture(uv_texture, uv_texcoord).rg;
     vec2 uv0 = texture(uv_texture, uv_texcoord + vec2(dx, 0.0)).rg;
     vec2 uv1 = texture(uv_texture, uv_texcoord + vec2(0.0, dy)).rg;
@@ -27,6 +34,15 @@ void main()
     float du = rU * lapl.r - uv.r * uv.g * uv.g + feed * (1.0 - uv.r);
     float dv = rV * lapl.g + uv.r * uv.g * uv.g - (feed + kill) * uv.g;
     vec2 dst = uv + vec2(du, dv);
+
+    if(brush.x > 0.0)
+    {
+        vec2 diff = (uv_texcoord - brush) / vec2(dx, dy);
+        float dist = dot(diff, diff);
+        if(dist < 1.0)
+        {  dst.g = 0.9;}
+
+    }
 
     result = vec4(dst, 0.0, 1.0);
 }
