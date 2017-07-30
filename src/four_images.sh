d66df741a5396a9c57af584674dde27d06b0d22e
cclauss <cclauss@bluewin.ch>
2017-07-30 16:29:22 +0200

    Shared directory between Docker host and container

--- src/four_images.sh
+++ src/four_images.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-echo "There should be no .png files when this script starts..."
+echo "There should be no .png files when this script starts."
 ls *.png
+echo "Running four pywonderland scripts..."
 python misc/e8.py
 python misc/mandelbrot.py
 python misc/modulargroup.py
 python misc/penrose.py
-echo "There should be four .png files when this script ends..."
+echo "There should be four .png files when this script ends."
 ls *.png
+echo "Copying all .png files to the shared directory."
+cp *.png /data_to_host
