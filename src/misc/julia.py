f2c9972d42d803e834259d71267a172eea023995
neozhaoliang <mathzhaoliang@gmail.com>
2017-08-03 10:38:46 +0800

    modify escape param in julia.py

--- src/misc/julia.py
+++ src/misc/julia.py
@@ -17,19 +17,19 @@ RADIUS = 4
 CONST = 0.7
 
 
-@jit('float32(complex64, complex64)')
-def escape(z, c):
+@jit('float32(complex64)')
+def escape(z):
     for i in range(MAXITERS):
         if z.real * z.real + z.imag * z.imag > RADIUS:
             break
-        z = (z*z + c) / (z*z - c)
+        z = (z*z + CONST) / (z*z - CONST)
     return i
 
 
 def main(xmin, xmax, ymin, ymax, width, height):
     y, x = np.ogrid[ymax: ymin: height*1j, xmin: xmax: width*1j]
     z = x + y*1j
-    img = np.asarray(np.frompyfunc(escape, 2, 1)(z, CONST)).astype(np.float)
+    img = np.asarray(np.frompyfunc(escape, 1, 1)(z)).astype(np.float)
     img /= np.max(img)
     img = np.sin(img**2 * np.pi) # a simple way to smooth the coloring.
     fig = plt.figure(figsize=(width/100.0, height/100.0), dpi=100)
