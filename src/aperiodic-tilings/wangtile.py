473f3c35a9269135bbe9c608647be477409b9956
neozhaoliang <mathzhaoliang@gmail.com>
2019-02-13 17:31:03 +0800

    fix a potential bug in texture.py/create_cubemap_texture

--- src/aperiodic-tilings/wangtile.py
+++ src/aperiodic-tilings/wangtile.py
@@ -55,6 +55,7 @@ class WangTile(pyglet.window.Window):
         gl.glBindTexture(gl.GL_TEXTURE_CUBE_MAP, cubemap)
         with self.shader:
             self.shader.vertex_attrib("position", [-1, -1, 1, -1, -1, 1, 1, 1])
+            self.shader.uniformf("iResolution", self.width, self.height, 0.0)
             self.shader.uniformf("iTime", 0.0)
             self.shader.uniformf("zoom", zoom)
             self.shader.uniformi("cubemap", 0)
@@ -65,7 +66,6 @@ class WangTile(pyglet.window.Window):
         gl.glViewport(0, 0, self.width, self.height)
         with self.shader:
             self.shader.uniformf("iTime", time.clock() - self._start_time)
-            self.shader.uniformf("iResolution", self.width, self.height, 0.0)
             gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 0, 4)
 
     def on_key_press(self, symbol, modifiers):
