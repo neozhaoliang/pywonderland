6b2a0d3dd44e15b2a37b7a3d9f6ea21d92e85994
neozhaoliang <mathzhaoliang@gmail.com>
2019-06-03 23:53:07 +0800

    add more Euclidean tiling examples

--- src/hyperbolic-tilings/helpers.py
+++ src/hyperbolic-tilings/helpers.py
@@ -1,51 +1,48 @@
-from collections import deque
+from fractions import Fraction
 import numpy as np
 
 
-def cdot(z, w):
-    return (z * w.conjugate()).real
-
-
-def ccross(z, w):
-    return (z * w.conjugate()).imag
-
-
-def make_symmetry_matrix(p, q, r):
-    return [[1, p, q], [p, 1, r], [q, r, 1]]
-
-
-def project_point(v, z1, z2):
-    """Project a point v to the line passes through z1 and z2
+def normalize(v):
+    """Normalize a vector `v`.
     """
-    n = z2 - z1
-    n /= abs(n)
-    t = cdot(v - z1, n)
-    return z1 + t * n
+    return np.array(v) / np.linalg.norm(v)
 
 
-def reflection_by_line(z1, z2):
-    """Return the reflection transformation about the line passes through points z1 and z2.
+def make_symmetry_matrix(upper_triangle):
+    """Given three or six integers/rationals, fill them into a
+       3x3 (or 4x4) symmetric matrix.
     """
-    return lambda v: 2 * project_point(v, z1, z2) - v
+    if len(upper_triangle) == 3:
+        a12, a13, a23 = upper_triangle
+        return [[1, a12, a13],
+                [a12, 1, a23],
+                [a13, a23, 1]]
+    elif len(upper_triangle) == 6:
+        a12, a13, a14, a23, a24, a34 = upper_triangle
+        return [[1, a12, a13, a14],
+                [a12, 1, a23, a24],
+                [a13, a23, 1, a34],
+                [a14, a24, a34, 1]]
+    else:
+        raise ValueError("Three or six inputs are expected.")
 
 
-def traverse(dfa, depth, reflections, fundamental_shape):
-    """Traverse the given dfa using bread-first search.
+def get_init_point(M, d):
+    """Given the normal vectors of the mirrors stored as row vectors in `M`
+       and a tuple of non-negative floats `d`, compute the vector `v` whose
+       distance vector to the mirrors is `d` and return its normalized version.
     """
-    queue = deque()
-    queue.append([dfa.start, (), 0, fundamental_shape] )
-    while queue:
-        state, word, i, shape = queue.popleft()
-        yield state, word, i, shape
-        if i < depth:
-            for symbol, target in state.all_transitions():
-                queue.append([target,
-                              word + (symbol,),
-                              i + 1,
-                              [reflections[symbol](p) for p in shape]])
+    return normalize(np.linalg.solve(M, d))
 
 
-def from_bary_coords(vertices, weights):
-    """Weighted sum of the points in `vertices`.
+def get_geometry_type(pqr):
+    """Get the geometry type. Return 1 if it's spherical, 0 if it's Euclidean,
+       -1 if it's hyperbolic.
     """
-    return np.dot(vertices, weights) / sum(weights)
+    s = sum([Fraction(1, x) if x != -1 else 0 for x in pqr])
+    if s > 1:
+        return 1
+    elif s == 1:
+        return 0
+    else:
+        return -1
--- src/hyperbolic-tilings/test.py
+++ src/hyperbolic-tilings/test.py
@@ -1,43 +1,32 @@
-"""
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Draw 2d Euclidean uniform tilings using the automata approach
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-There are three kinds of uniform tilings in the 2d Euclidean plane,
-their symmetry groups are all Coxeter groups of affine type:
-
-1. Tiling (3, 3), its group is affine A_2~.
-2. Tiling (4, 4), its group is affine B_2~.
-3. Tiling (6, 3), its group is affine G_2~.
-"""
-from itertools import combinations
+import collections
+import itertools
 from functools import partial
-from collections import deque
-import numpy as np
 import cairocffi as cairo
-import helpers
+import numpy as np
 import automata
+import helpers
 
 
-def hex_to_rgb(value):
-    """Hex value to (r, g, b) triple.
-    """
+def color(value):
     return [((value >> (8 * k)) & 255) / 255.0 for k in (2, 1, 0)]
+
+colors = [0x477984, 0xEEAA4D, 0xC03C44, 0xFEF5EB]
 
 
 def inset_corners(points, margin):
     def get_point(p, q):
         vx = p[0] - q[0]
         vy = p[1] - q[1]
-        r = np.sqrt(vx * vx + vy * vy)
+        r = np.sqrt(vx*vx + vy*vy)
         t = 1 - margin / r
         return q[0] + t * vx, q[1] + t * vy
 
     q = np.sum(points, axis=0) / len(points)
+
     return [get_point(p, q) for p in points]
 
 
-class EuclideanUniformTiling(object):
+class Tiling(object):
 
     def __init__(self, pqr, init_dist):
         self.active = [bool(x) for x in init_dist]
@@ -60,15 +49,14 @@ class EuclideanUniformTiling(object):
         return M
 
     def get_reflections(self, mirrors, init_dist):
-        def reflect(v, normal, dist):
-            """Affine reflections.
-            """
-            return v - 2 * (np.dot(v, normal) + dist) * normal
+        def reflect(v, N, d):
+            return v - 2 * (np.dot(v, N) + d) * N
 
-        return [partial(reflect, normal=n, dist=d) for n, d in zip(mirrors, init_dist)]
+        return [partial(reflect, N=N, d=d) for N, d in zip(mirrors, init_dist)]
+
 
     def get_fundamental_faces(self):
-        for i, j in combinations(range(3), 2):
+        for i, j in itertools.combinations(range(3), 2):
             f0 = []
             P = self.init_v
 
@@ -101,7 +89,7 @@ class EuclideanUniformTiling(object):
     def traverse(self, depth, domain):
         """Traverse the dfa using breath-first search up to a given depth.
         """
-        queue = deque()
+        queue = collections.deque()
         queue.append([self.dfa.start, (), 0, domain])
 
         while queue:
@@ -115,60 +103,35 @@ class EuclideanUniformTiling(object):
                                   steps + 1,
                                   [self.reflections[symbol](p) for p in shape]])
 
-    def draw(self, output, image_width, image_height,
-             extent, depth, line_width=0.2, margin=0.3,
-             edge_color=0x313E4A, face_colors=[0x477984, 0xEEAA4D, 0xC03C44]):
+    def draw(self, image_width, image_height, extent, depth=20):
         surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, image_width, image_height)
         ctx = cairo.Context(surface)
         ctx.scale(image_height / extent, image_height / extent)
         ctx.translate(extent / 2, extent / 2)
         ctx.set_source_rgb(0, 0, 0)
         ctx.paint()
-        ctx.set_line_width(line_width)
+        ctx.set_line_width(0.3)
         ctx.set_line_cap(cairo.LINE_CAP_ROUND)
         ctx.set_line_join(cairo.LINE_JOIN_ROUND)
 
-        self.get_fundamental_faces()
         for i, face in enumerate(self.fundamental_faces):
             for _, _, _, shape in self.traverse(depth, face):
                 shape = [self.project_to_plane(v) for v in shape]
-                shape = inset_corners(shape, margin)
-                p = shape[0]
-                ctx.move_to(p[0], p[1])
-                for q in shape[1:]:
-                    ctx.line_to(q[0], q[1])
-                ctx.line_to(p[0], p[1])
-                if face_colors is not None:
-                    ctx.set_source_rgb(*hex_to_rgb(face_colors[i]))
-                    ctx.fill_preserve()
-                ctx.set_source_rgb(*hex_to_rgb(edge_color))
+                shape = inset_corners(shape, 0.3)
+                z = shape[0]
+                ctx.move_to(z[0], z[1])
+                for w in shape[1:]:
+                    ctx.line_to(w[0], w[1])
+                ctx.line_to(z[0], z[1])
+                ctx.set_source_rgb(*color(colors[i]))
+                ctx.fill_preserve()
+                ctx.set_source_rgb(*color(0x313E4A))
                 ctx.stroke()
 
-        surface.write_to_png(output)
+        surface.write_to_png("Euclidean.png")
 
 
-def main():
-    width, height = 1200, 960
-
-    T = EuclideanUniformTiling((3, 3, 3), (1, 0, 0))
-    T.draw("333-100.png", width, height, extent=10, depth=30,
-           face_colors=[0x477984, 0x477984])
-
-    T = EuclideanUniformTiling((3, 3, 3), (1, 1, 0))
-    T.draw("333-110.png", width, height, extent=20, depth=30,
-           face_colors=[0xEEAA4D, 0x477984, 0x477984])
-
-    T = EuclideanUniformTiling((2, 4, 4), (1, 1, 1))
-    T.draw("244-111.png", width, height, extent=30, depth=30)
-
-    T = EuclideanUniformTiling((2, 4, 4), (0, 1, 1))
-    T.draw("244-011.png", width, height, extent=25, depth=30)
-
-    T = EuclideanUniformTiling((2, 3, 6), (1, 0, 1))
-    T.draw("236-101.png", width, height, extent=30, depth=30)
-
-    T = EuclideanUniformTiling((2, 3, 6), (1.2, 1.0, 0.8))
-    T.draw("236-111.png", width, height, extent=30, depth=50)
-
-if __name__ == "__main__":
-    main()
+T = Tiling((2, 3, 6), (1, 1, 1))
+T.get_fundamental_faces()
+T.draw(1200, 960, 50, 80)
+T.dfa.draw("automata.png")
