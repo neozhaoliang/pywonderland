e56d599f4291f2dc66bb9689dac074f480c1ef18
neozhaoliang <mathzhaoliang@gmail.com>
2022-01-19 00:27:36 +0800

    remove unused axes3d import

--- src/misc/lorenz.py
+++ src/misc/lorenz.py
@@ -11,7 +11,6 @@ code adapted from
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
-from mpl_toolkits.mplot3d import Axes3D
 from scipy.integrate import odeint
 
 # number of particles.
